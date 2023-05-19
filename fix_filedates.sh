58a049965a9e30967fffde4ed333b2da4d459d03
Chris Tracy <ctracy@es.net>
2023-05-18 21:33:24 -0700

    comments

--- fix_filedates.sh
+++ fix_filedates.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+# use something like this to change dates if camera is way off:
+# exiftool "-AllDates+=6:11:26 0" .
+# shifts by 6 years, 11 months, 26 days
+# see cheatsheet at https://ryanmo.co/2014/09/28/exiftool-cheatsheet/
+
 /opt/local/bin/exiftool "-DateTimeOriginal>FileModifyDate" *
